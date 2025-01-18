fcd8e25509952de39737486153b194525de8f0f0
Azamat Hakimov <azamathakimovdev@gmail.com>
2025-01-18 03:16:36 +0300

    Initial commit: Set up project structure and uploaded basic files.

---
+++ src/main/java/com/portfolio/movieapi/MovieRepository.java
@@ -0,0 +1,12 @@
+package com.portfolio.movieapi;
+
+import java.util.Optional;
+
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MovieRepository extends MongoRepository<Movie, ObjectId>{
+    Optional<Movie> findByImdbId(String imdbId);
+}
